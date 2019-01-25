9c8ee22bef0df95f55e352af969035f4b6d06945
Luis Constante <lconstante@viscira.com>
2019-01-24 16:02:25 -0800

    building out home and search views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
+import {ListComponent} from './views/results/list/list.component';
+import {MapComponent} from './views/results/map/map.component';
 
 const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full' },
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'list', component: ListComponent},
+    {path: 'map', component: MapComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavigationComponent } from './globals/navigation/navigation.component';
 import { HomeComponent } from './views/home/home.component';
+import { ListComponent } from './views/results/list/list.component';
+import { MapComponent } from './views/results/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent
+    HomeComponent,
+    ListComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [
   ],
