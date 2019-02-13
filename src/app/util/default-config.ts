a84150381f1498fcfd73af27393b744b9c08cf97
Luis Constante <lconstante@viscira.com>
2019-02-12 16:52:52 -0800

    configs

---
+++ src/app/util/default-config.ts
@@ -0,0 +1,14 @@
+import {onAuthRequired} from '../app/app-routing.module';
+
+export default {
+    oidc: {
+        clientId: '0oajcymqn9CrPFLSd0h7',
+        issuer: 'https://dev-298781.oktapreview.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/implicit/callback',
+        scope: 'openid profile email',
+        onAuthRequired: onAuthRequired
+    },
+    resourceServer: {
+        messagesUrl: 'http://localhost:8000/api/messages',
+    },
+};
