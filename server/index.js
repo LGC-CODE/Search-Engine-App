923bf6e0d77f2a7d8baed93f98351294d4032875
Luis Constante <lconstante@viscira.com>
2019-02-23 01:09:19 -0800

    checkpoint

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@
 const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
-// const server = app.listen(port);
+const server = app.listen(port);
 const https = require('https');
 const fs = require('fs');
-
+/*
 var privateKey = fs.readFileSync(__dirname + '/config/incyte.key', 'utf8');
 var certificate = fs.readFileSync(__dirname + '/config/incyte.cert', 'utf8');
 
@@ -16,12 +16,12 @@ const httpsServer = https.createServer({
 
 const serverHttps = httpsServer.listen(port);
 
-app.setup(serverHttps);
+app.setup(serverHttps);*/
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-serverHttps.on('listening', () =>
+server.on('listening', () =>
   logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
 );
